e1aaf316e73c83fd43f59cf881a85c09652e8454
rzhu004 <robert.zhu@alcatel-lucent.com>
2014-09-08 16:45:39 -0700

    first check in

---
+++ mian/java/Azhu/UniqueCharacter.java
@@ -0,0 +1,13 @@
+
+public class UniqueCharacter {
+	public static boolean isUnique(String x){
+		for (int i=0; i<x.length(); i++) {
+			char current = x.charAt(i);
+			
+		}
+		return false;
+	}
+	public static void main(String[] args){
+		
+	}
+}
