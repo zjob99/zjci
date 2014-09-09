020a631bb5bf21daf7864d3aa64ca104aeccca87
rzhu004 <robert.zhu@alcatel-lucent.com>
2014-09-08 23:50:28 -0700

    add new files

---
+++ mian/java/Azhu/CountContinuousDup.java
@@ -0,0 +1,30 @@
+//convert aabbbc to a2b3c1, if the converted string is longer, return itself
+public class CountContinuousDup {
+	public static String cvtString(String s) {
+		StringBuilder sb=new StringBuilder();
+		char[] tmp = s.toCharArray();
+		char pre = s.charAt(0);int ct=0;char cur=pre;
+		for (int i=0; i<s.length(); i++){
+			char cc=s.charAt(i);
+			if (cc==pre){
+				ct++;
+				System.out.println(cc+":"+ct);
+			}
+			else {
+
+				sb.append(String.valueOf(pre)+":" + String.valueOf(ct)+", ");
+				pre=cc;
+				ct=1;
+			}
+			if (i==s.length()-1){
+				sb.append(String.valueOf(cc)+":"+String.valueOf(ct));
+			}
+				
+		}
+		return sb.toString();
+	}
+	public static void main(String[] args){
+		System.out.println(CountContinuousDup.cvtString("abbbccde"));
+		
+	}
+}
---
+++ mian/java/Azhu/PermuTation.java
@@ -0,0 +1,17 @@
+
+public class PermuTation {
+
+	public static String sort(String s) {
+		char[] content = s.toCharArray();
+		java.util.Arrays.sort(content);
+		return new String(content);
+	}
+	
+	public static boolean isPermutation(String s, String t) {
+		return sort(s).equals(sort(t));
+	}
+	public static void main(String[] args){
+		System.out.println(PermuTation.isPermutation("12345", "34521"));
+		
+	}
+}
